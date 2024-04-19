8fa39bbcaaf5587948752dd0e112a65ec77c0fea
Graeme Stirling <graemejstirling@gmail.com>
2024-04-19 19:50:57 +0100

    working on taking checkers

--- src/hooks/tests/useBoardState.test.ts
+++ src/hooks/tests/useBoardState.test.ts
@@ -1,13 +1,40 @@
 import { describe, expect, test } from "vitest";
 import {canMove, initialBoard, calcNewBoard} from "../useBoardState";
 
+type BoardLayout = (string | null)[][];
+
+const testBoard: BoardLayout = [
+    [null, "blue", null, "blue", null, "blue", null, "blue"],
+    ["blue", null, "blue", null, "blue", null, "blue", null],
+    [null, "blue", null, null, null, "blue", null, "blue"],
+    [null, null, "blue", null, null, null, null, null],
+    [null, "red", null, null, null, null, null, null],
+    ["red", null, null, null, "red", null, "red", null],
+    [null, "red", null, "red", null, "red", null, "red"],
+    ["red", null, "red", null, "red", null, "red", null],
+  ];
+
 describe("canMove: ", () => {
     // true cases:
-    test('Returns true when checker is selected and target is correct', () => {
+    test('Returns true when red checker is selected and target is correct', () => {
         const result = canMove([5,2], [4,3], initialBoard, true)
         
         expect(result).toBe(true)
     }); 
+
+    test('Returns true when blue checker is selected and target is correct', () => {
+        const result = canMove([2,3], [3,2], initialBoard, false)
+        
+        expect(result).toBe(true)
+    }); 
+
+    // in [progress]
+    test('Returns true when red checker is selected, target is two moves away and there is a blue checker in path', () => {
+        const result = canMove([4,1], [2,3], testBoard, false)
+        
+        expect(result).toBe(true)
+    }); 
+    
     // False cases
     test('Returns false when no checker is selected', () => {
         const result = canMove(null, [4,3], initialBoard, true)
@@ -32,6 +59,30 @@ describe("canMove: ", () => {
         
         expect(result).toBe(false)
     });
+
+    test('Returns false when red chekcer target is two rows away', () => {
+        const result = canMove([5,2], [3,4], initialBoard, true)
+        
+        expect(result).toBe(false)
+    }); 
+
+    test('Returns false when blue checker target is two rows away', () => {
+        const result = canMove([2,3], [4,1], initialBoard, false)
+        
+        expect(result).toBe(false)
+    }); 
+
+    test('Returns false on red turn when target has a blue checker in path', () => {
+        const result = canMove([4,1], [3,2], testBoard, false)
+        
+        expect(result).toBe(false)
+    }); 
+
+    test('Returns false on blue turn when target has a red checker in path', () => {
+        const result = canMove([3,2], [4,1], testBoard, true)
+        
+        expect(result).toBe(false)
+    }); 
 });
 
 describe("calcNewBoard: ", () => {
--- src/hooks/useBoardState.ts
+++ src/hooks/useBoardState.ts
@@ -14,6 +14,16 @@ export const initialBoard: BoardLayout = [
   ["red", null, "red", null, "red", null, "red", null],
 ];
 
+const testBoard: BoardLayout = [
+  [null, "blue", null, "blue", null, "blue", null, "blue"],
+  ["blue", null, "blue", null, "blue", null, "blue", null],
+  [null, "blue", null, null, null, "blue", null, "blue"],
+  [null, null, "blue", null, null, null, null, null],
+  [null, "red", null, null, null, null, null, null],
+  ["red", null, null, null, "red", null, "red", null],
+  [null, "red", null, "red", null, "red", null, "red"],
+  ["red", null, "red", null, "red", null, "red", null],
+];
 /**
  *  Can move checks the  move is legal and returns a boolean based on this information
  */
@@ -33,9 +43,16 @@ export function canMove(
   const checkerColour = board[selectedChecker[0]][selectedChecker[1]];
   const isRedTurn = !turn && checkerColour === "red";
   const isBlueTurn = turn && checkerColour === "blue";
-
   if (isRedTurn || isBlueTurn) return false;
 
+  // if checker on square +1y (-1 or +1 x) then allow move
+  // override if checker is between
+
+  // is turn two moves away ?
+
+// enforcing 1 step rule
+  if (selectedChecker[0] - target[0] > 1) return false;
+  if (target[0] - selectedChecker[0] > 1) return false;
   return true;
 }
 
@@ -58,7 +75,7 @@ export function calcNewBoard(
 }
 
 export function useBoardState() {
-  const [board, setBoard] = useState<BoardLayout>(initialBoard);
+  const [board, setBoard] = useState<BoardLayout>(testBoard);
   const [selectedChecker, setSelectedChecker] = useState<Coordinates | null>(
     null
   );
